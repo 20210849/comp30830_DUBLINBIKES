a6710c61a2c6d5ffdced0b5bea4549abf9ee3264
21201607 <95704600+21201607@users.noreply.github.com>
2022-03-30 17:45:15 +0100

    weather

--- DataScrapt/weatherScraper.py
+++ DataScrapt/weatherScraper.py
@@ -1,49 +1,42 @@
 import pymysql 
 import traceback
-import glob
-import os
-from pprint import pprint
 import requests
 import time
-from IPython.display import display
-from IPython.display import JSON
 from datetime import datetime
 import json
-import pandas as pd
 
-db = pymysql.connect(
-    host="dbbikes1.citjnbrbkplf.us-east-1.rds.amazonaws.com",
-    user="admin",
-    password="12345678",
-    port=3306,
-    database="dbbikes1")
-cursor = db.cursor()   
+weather_apiKey = "c52ab5b9c342a6c8e1bf83b83bd3313a"
+city_name = 'Dublin,ie'
+parameters = {"q" : city_name, "appid" : weather_apiKey} 
+weather_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-sql = """
-CREATE TABLE IF NOT EXISTS weather_Dublin(
-    Clouds INTEGER,
-    feels_like DOUBLE,
-    humidity INTEGER,
-    pressure INTEGER,
-    temp DOUBLE,
-    temp_max DOUBLE,
-    temp_min DOUBLE,
-    sunrise VARCHAR(255),
-    sunset VARCHAR(255),
-    visibility INTEGER,
-    weather_description VARCHAR(255),
-    weather_main VARCHAR(255),
-    wind_deg INTEGER,
-    wind_speed DOUBLE,
-    dt VARCHAR(255)
-);
-"""
-try:
-    res = cursor.execute(sql)
-    print('ok')
-except Exception as e:
-    print(e)
-
+def create_table():
+    sql = """
+    CREATE TABLE IF NOT EXISTS weather_Dublin(
+        Clouds INTEGER,
+        feels_like DOUBLE,
+        humidity INTEGER,
+        pressure INTEGER,
+        temp DOUBLE,
+        temp_max DOUBLE,
+        temp_min DOUBLE,
+        sunrise VARCHAR(255),
+        sunset VARCHAR(255),
+        visibility INTEGER,
+        weather_description VARCHAR(255),
+        weather_main VARCHAR(255),
+        wind_deg INTEGER,
+        wind_speed DOUBLE,
+        dt VARCHAR(255)
+    );
+    """
+        
+    try:
+        res = cursor.execute(sql)
+        print('create ok')
+    except Exception as e:
+        print(e)
+        
 
 def write_to_db(text):
     weather_data = json.loads(text)
@@ -66,7 +59,7 @@ def write_to_db(text):
         str(datetime.fromtimestamp(weather_data['dt']))
     )
     
-    sql = """INSERT INTO dbbikes.weather_Dublin (Clouds,feels_like,humidity,pressure,
+    sql = """INSERT INTO dbbikes1.weather_Dublin (Clouds,feels_like,humidity,pressure,
     temp,temp_max,temp_min,sunrise,sunset,visibility,weather_description,
     weather_main,wind_deg,wind_speed,dt) 
     VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',
@@ -81,26 +74,27 @@ def write_to_db(text):
         print("insert wrong")
     
     db.close()
-
-
-def main():
-    weather_apiKey = "1b2d168422195fdadf28f15d5f1dba4e"
-    city_name = 'Dublin,ie'
     
-    parameters = {"q" : city_name, "appid" : weather_apiKey} 
-    
-    weather_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-    while True:
-        try:
-            now = datetime.now()
-            r = requests.get(weather_URL, params = parameters)
-            write_to_db(r.text)
+while True:
+    try:
+        db = pymysql.connect(
+            host="dbbikes1.citjnbrbkplf.us-east-1.rds.amazonaws.com",
+            user="admin",
+            password="12345678",
+            port=3306,
+            database="dbbikes1")
+        cursor = db.cursor()  
             
-            time.sleep(5*60)
-        except:
-            print(traceback.format_exc())
+        create_table()
 
-
-if __name__ == "__main__":
-    main()
+        now = datetime.now()
+        r = requests.get(weather_URL, params = parameters)
+        print(r,now)
+            
+        write_to_db(r.text)
+            
+        time.sleep(5*60)
+        
+    except:
+        print(traceback.format_exc())    
